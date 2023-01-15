003a5764cf1ad663ec51b96a1a31aaf00adc5d09
Vadym <besarabvadim2@gmail.com>
2023-01-15 16:00:02 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 
-
-
 export const ContactList = ({ contacts, onClick }) => {
   return (
-    <div >
+    <div>
       {contacts.map(contact => {
         return (
-            <div className = "ContactListThumb"  key={nanoid()}>
+          <div className="ContactListThumb" key={nanoid()}>
             <p>
               {contact.name}: {contact.number}
             </p>
-            <button className = "button"
+            <button
+              className="button"
               type="button"
               onClick={() => {
                 onClick(contact.id);
@@ -29,11 +28,11 @@ export const ContactList = ({ contacts, onClick }) => {
 };
 
 ContactList.propTypes = {
-    contacts: PropTypes.arrayOf(
-        PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            number: PropTypes.string.isRequired,
-        })
-    ),
-    onClick: PropTypes.func.isRequired
-}
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onClick: PropTypes.func.isRequired,
+};
